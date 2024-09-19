ca4df1a116c8d292f0a4f6b2e356444e46699be0
Shravyasri1703 <shravya.kmp@gmail.com>
2024-09-19 09:04:15 +0530

    cleaned up

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,15 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import { CiHeart } from "react-icons/ci";
 import { SlHandbag } from "react-icons/sl";
 import { CiBellOn } from "react-icons/ci";
 
 const Navbar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const toggleSidebar = () => {
-    setIsSidebarOpen(prev => !prev);
-  };
+  
   return (
     <nav className={`flex flex-row w-screen h-12 bg-black top-0  items-center p-2`}>
         <div className='flex flex-row h-full w-1/2 items-center gap-7 ml-6'>
-            <h1 className={`text-2xl font-extrabold text-gray-300 ${isSidebarOpen ? 'hidden' : ''}`}>Quantum</h1>
+            <h1 className={`text-2xl font-extrabold text-gray-300 `}>Quantum</h1>
         </div>
         <div className='flex flex-row h-full w-1/2 items-center gap-10 justify-end mr-16'>
          <CiHeart size={25} color='white'/>
